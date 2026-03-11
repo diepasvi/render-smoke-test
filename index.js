316fc01151ac1adf132e8cca45cdb42ce5ee4f5f
diepasvi <diepasvi@gmail.com>
2026-03-11 12:18:07 +0100

    Update smoke test response message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>¡Hola desde Renderizado! 🚀</h1><p>El smoke test ha funcionado correctamente.</p>');
+  res.send('<h1>¡Hola desde Renderizado! 🚀</h1><p>El smoke test ha funcionado correctamente. Eres un crack!!</p>');
 });
 
 app.listen(PORT, () => {
